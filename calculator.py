70b2ef283c536a88c86cabc7e1ffd84b96162eb8
Ven_Choi <yuventius125@gmail.com>
2026-06-25 10:31:13 +0900

    ✨ Feat(calculator.py): operations dictionary updated
    - multiply
    - divide

--- calculator.py
+++ calculator.py
@@ -1,9 +1,13 @@
 from ops.add import add
 from ops.subtract import subtract
+from ops.multiply import multiply
+from ops.divide import divide
 from ops.power import power
 
 operations = {
     "add": add,
     "subtract": subtract,
+    "multiply": multiply,
+    "divide": divide,
     "power": power,
 }
