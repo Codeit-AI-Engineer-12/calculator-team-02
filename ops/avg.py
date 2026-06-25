7ebe7a3870908332a4f76b8765b8a41b39d07e62
Ven_Choi <yuventius125@gmail.com>
2026-06-25 14:46:22 +0900

    Feat: avg 함수 업데이트 Feat: root 함수 업데이트

--- ops/avg.py
+++ ops/avg.py
@@ -1,2 +1,13 @@
 def avg(a, b):
-    pass
+    """
+    a와 b의 평균값을 계산합니다.
+    :param a: arg1
+    :param b: arg2
+    :return: arg1과 arg2의 평균값
+    """
+    try:
+        return (a + b) / 2
+    except TypeError as e:
+        raise TypeError(f"Argument is not numeric: {e}") from e
+    except OverflowError as e:
+        raise OverflowError(f"Numerical overflow: {e}") from e
--- ops/root.py
+++ ops/root.py
@@ -1,2 +1,17 @@
 def root(a, b):
-    pass
+    """
+    a를 b로 거듭제곱근합니다.
+    :param a: arg1
+    :param b: arg2
+    :return: 거듭제곱근 연산 결과
+    """
+    try:
+        if a < 0 and b % 2 == 0:
+            raise ValueError("arg1 cannot be negative and arg2 must be odd")
+        return a ** (1/b)
+    except ZeroDivisionError as e:
+        raise ZeroDivisionError(f"arg2 cannot be zero: {e}") from e
+    except TypeError as e:
+        raise TypeError(f"Argument is not numeric: {e}") from e
+    except OverflowError as e:
+        raise OverflowError(f"Numerical overflow: {e}") from e
